c06770a8779bef96e00797f9304d7b29b7ab39f3
Kim Hwanhoon <kimhwanhoon@outlook.com>
2023-06-28 20:55:14 +0900

    20230628 진솔님, 환훈님

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,41 @@
-import React from "react";
-import AddTodo from "../components/AddTodo";
-import PaintTodo from "../components/PaintTodo";
-import { useSelector } from "react-redux";
-import S from "../globalStyle";
+import React from 'react';
+import AddTodo from '../components/AddTodo';
+import PaintTodo from '../components/PaintTodo';
+import { useDispatch, useSelector } from 'react-redux';
+import S from '../globalStyle';
+import { toggleThemeee } from '../redux/modules/themeReducer';
 
-const Home = () => {
+const Home = ({ theme, setTheme }) => {
   const { todos } = useSelector((state) => {
     return state.todoReducer;
+  });
+  const dispatch = useDispatch();
+  const test = useSelector((state) => {
+    return state.themeReducer.darkmode;
   });
   return (
     <div>
       <S.Header>
         <S.HeaderSpan>My Todo List</S.HeaderSpan>
-        <S.HeaderSpan>React</S.HeaderSpan>
+        <div>
+          <select name="" id="">
+            <option value="1">작게</option>
+            <option value="2">중간</option>
+            <option value="3">크게</option>
+          </select>
+          <button
+            onClick={() => {
+              dispatch(toggleThemeee(test));
+            }}
+          >
+            {theme === 'light' ? 'dark' : 'light'}
+          </button>
+        </div>
       </S.Header>
       <AddTodo />
       <S.PaintDiv>
         <S.PaintDivH1>Working🔥</S.PaintDivH1>
-        <div style={{ display: "flex", flexWrap: "wrap" }}>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
           {todos
             .filter((todo) => !todo.isDone)
             .map((todo) => {
@@ -27,7 +45,7 @@ const Home = () => {
       </S.PaintDiv>
       <S.PaintDiv>
         <S.PaintDivH1>Done🎉</S.PaintDivH1>
-        <div style={{ display: "flex", flexWrap: "wrap" }}>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
           {todos
             .filter((todo) => todo.isDone)
             .map((todo) => {
--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,8 +1,9 @@
-import { createStore } from "redux";
-import { combineReducers } from "redux";
-import todoReducer from "./modules/todoReducer";
+import { createStore } from 'redux';
+import { combineReducers } from 'redux';
+import todoReducer from './modules/todoReducer';
+import themeReducer from './modules/themeReducer';
 
-const rootReducer = combineReducers({ todoReducer });
+const rootReducer = combineReducers({ todoReducer, themeReducer });
 const store = createStore(rootReducer);
 
 export default store;
